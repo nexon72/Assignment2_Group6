6c24307f051e1ad407851396569814737633819d
Raphael <Raphael@DESKTOP-KITV644>
2020-02-23 22:03:27 -0500

    Comments updated. Ready for submission

--- Assignment2_Group6/Program.cs
+++ Assignment2_Group6/Program.cs
@@ -9,13 +9,13 @@ namespace Assignment2_Group6
         {
             //QUESTION 1
             Console.WriteLine("Question 1");
-            int[] l1 = new int[] {5,6,6,9,9,12};
+            int[] l1 = new int[] { 5, 6, 6, 9, 9, 12 };
             //int target = 9;
             int target = 10;
             int[] r = TargetRange(l1, target);
             Console.WriteLine(string.Join(" ", r));
             Console.WriteLine();
-              
+
             //QUESTION 2
             Console.WriteLine("Question 2");
             string s = "University of South Florida";
@@ -25,9 +25,9 @@ namespace Assignment2_Group6
             //QUESTION 3
             Console.WriteLine();
             Console.WriteLine("Question 3");
-            int[] l2 = new int[] {2,2,3,5,6};
-          //int[] l2 = new int[] {40,40};
-          //int[] l2 = new int[] {4,5,6,9};
+            int[] l2 = new int[] { 2, 2, 3, 5, 6 };
+            //int[] l2 = new int[] {40,40};
+            //int[] l2 = new int[] {4,5,6,9};
             int sum = MinimumSum(l2);
             Console.WriteLine(sum);
 
@@ -43,10 +43,10 @@ namespace Assignment2_Group6
             //QUESTION 5 - PART 1
             Console.WriteLine();
             Console.WriteLine("Question 5-Part 1");
-          //int[] nums1 = { 2,5,5,2};
-          //int[] nums2 = { 5,5 };
-            int[] nums1 = {3,6,2};
-            int[] nums2 = {6,3,6,7,3};
+            //int[] nums1 = { 2,5,5,2};
+            //int[] nums2 = { 5,5 };
+            int[] nums1 = { 3, 6, 2 };
+            int[] nums2 = { 6, 3, 6, 7, 3 };
 
             int[] intersect1 = Intersect1(nums1, nums2);
 
@@ -70,7 +70,6 @@ namespace Assignment2_Group6
             //int k = 2;
             //char[] arr = new char[] { 'k', 'y', 'k', 'k' };
             //int k = 1;
-
             Console.WriteLine(ContainsDuplicate(arr, k));
 
             //QUESTION 7 
@@ -100,114 +99,96 @@ namespace Assignment2_Group6
                 Console.Write(n + " ");
             }
         }
-
-
         public static int[] TargetRange(int[] l1, int t)
         {
-            bool found = false;
-            int index = 0;
-            int position = -1;
-            int position2 = -1;
+            bool found = false;                             // declare boolean variable 'found' as false
+            int index = 0;                                  // declare integer variable 'index' to 0
+            int position = -1;                              // set integer variable 'position' to -1
+            int position2 = -1;                             // set integer variable 'position2' to -1
 
             try
             {
-                //Scan through the array 
                 while (index < l1.Length)
-                {
+                {                 // scan through the array while index is less than the length of array 'l1'
                     if (l1[index] == t && !found)
-                    {
-                        found = true;
-                        position = index;
+                    {         // when target is found
+                        found = true;                        // set 'found' to True
+                        position = index;                    // set position (start index) as the index of that number
                     }
-
                     if (l1[index] == t && found == true)
                     {
-                        position2 = index;
+                        position2 = index;                  // set position2 (end index) as the index of that number
                     }
-
                     index++;
                 }
             }
-            catch (Exception)
-            {
-                throw;
-            }
-
+            catch (Exception) { throw; }                    // throw an error if index is more than the length of given array i.e. the 'marks' array
             return new int[] { position, position2 };
         }
-
-
         public static string StringReverse(string s)
         {
             try
             {
-                string final_str = "", temp_str = "";
-                for (int i = s.Length-1; i > -1; i--)
-                {
-
+                string final_str = "", temp_str = "";       // declare string variables 'final_str' and 'temp_str'
+                for (int i = s.Length - 1; i > -1; i--)
+                {     // traverse through the length of array in reverse
                     //Console.WriteLine(s[i]);
                     if (!s[i].Equals(' '))
-                    {
-                        temp_str = temp_str + s[i];
+                    {                // checking for spaces
+                        temp_str = temp_str + s[i];         // concatanate the character to 'temp_str'
                     }
                     else
                     {
-                        final_str = temp_str + " " + final_str;
+                        final_str = temp_str + " " + final_str;     // concatanate the 'final_str'
                         temp_str = "";
                     }
                 }
-                final_str = temp_str + " " + final_str;
-                Console.WriteLine(final_str);
+                final_str = temp_str + " " + final_str;     // reversed word
+                Console.WriteLine(final_str);               // display reversed word
 
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            catch (Exception) { throw; }                    // throw an error if length of the string is 0
             return null;
         }
-
         public static int MinimumSum(int[] l2)
         {
-            int newSum = 0;
-
+            int newSum = 0;                                         // declare integer variable 'newSum' to 0
             try
             {
-                for (int i = 0; i < l2.Length; i++)
-                {
+                for (int i = 0; i < l2.Length; i++)                 // for 0 to length of array 'l2'
+                {              
                     if (i != l2.Length - 1 && l2[i] == l2[i + 1])
-                    {
-                        l2[i + 1] = l2[i] + 1;
+                    { // 
+                        l2[i + 1] = l2[i] + 1;                     
                     }
-
                     newSum += l2[i];
                 }
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            catch (Exception) { throw; }
             return newSum;
         }
-
         public static string FreqSort(string s2)
         {
             try
             {
                 Dictionary<char, int> freqKey = new Dictionary<char, int>();
-
+                //for loop to traverse each character in the string
                 for (int i = 0; i < s2.Length; i++)
                 {
+                    //Condition checks if the key is not resent in the dictionary
                     if (!freqKey.ContainsKey(s2[i]))
                     {
+                        //Adds the jey in the dictionary with a value count as 1
                         freqKey.Add(s2[i], 1);
                     }
                     else
                     {
+                        //Increases the value count by 1  of the existig key in the dictionary
                         freqKey[s2[i]] += 1;
                     }
                 }
 
+                //for loop to to print the key value pairs in the descending order of cout value.
                 foreach (var item in freqKey.OrderByDescending(x => x.Value))
                 {
                     for (int d = 0; d < item.Value; d++)
@@ -216,34 +197,27 @@ namespace Assignment2_Group6
                     }
                 }
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            catch (Exception) { throw; }
             return null;
         }
-
         public static int[] Intersect1(int[] nums1, int[] nums2)
         {
             try
             {
-                //Intialising the all the variables
-                int a = nums1.Length;
-                int b = nums2.Length;
-                int i = 0;
-                int j = 0;
+                int a = nums1.Length, b = nums2.Length, i = 0,j=0;
+
                 //Sorting the both the arrays
                 Array.Sort(nums1);
                 Array.Sort(nums2);
-                //Declaring the out1 list object to capture the intersection elements
-                List<int> out1 = new List<int>();
-                //while lopp to get the intersection elements of both arrays
+                //Declaring the output1 list object to store the intersection elements
+                List<int> output1 = new List<int>();
+                //while loop to get the intersection elements of both arrays
                 while (i < a && j < b)
                 {
                     if (nums1[i] == nums2[j])
                     {
-                        //adding the common element to out1 list variable
-                        out1.Add(nums1[i]);
+                        //adding the common element to output1 list variable
+                        output1.Add(nums1[i]);
                         i++;
                         j++;
                     }
@@ -259,61 +233,59 @@ namespace Assignment2_Group6
                     }
                 }
                 //returning the array of intersection elements
-                return out1.ToArray();
+                return output1.ToArray();
             }
             //Below block will be executed if any exception occurs
-            catch
-            {
-                throw;
+            catch 
+            { 
+                throw; 
             }
         }
-
+        
         //This method to print the common elements of two arrays with time complexity of O(n)
-
         public static int[] Intersect2(int[] nums1, int[] nums2)
         {
             try
             {
                 //getting the lengths of arrays
-                int a = nums1.Length;
-                int b = nums2.Length;
+                int a = nums1.Length, b = nums2.Length;
                 //adding the arrays
                 List<int> result = new List<int>();
                 //intialising the new dictionary variable
                 Dictionary<int, int> dict = new Dictionary<int, int>();
                 //for loop to traverse through first array
-                foreach (int value in nums1)
+                foreach (int val in nums1)
                 {
                     //condition to check dictinary contains the first array element or not
-                    if (dict.ContainsKey(value))
+                    if (dict.ContainsKey(val))
                     {
                         //Incrementing the value if it contains key
-                        dict[value]++;
+                        dict[val]++;
                     }
                     else
                     {
-                        dict[value] = 1;
+                        dict[val] = 1;
                     }
                 }
                 //for loop to traverse through second array elements
                 for (int i = 0; i < b; i++)
                 {
                     //intialising the count variable to 0
-                    int count = 0;
+                    int counter = 0;
                     //condition to check dictionary contains second array element or not
                     if (dict.ContainsKey(nums2[i]))
                     {
                         //assiging the value of array element to count variable
-                        count = dict[nums2[i]];
+                        counter = dict[nums2[i]];
                         //condition to check count greater than 1 or not
-                        if (count > 1)
+                        if (counter > 1)
                         {
                             //adding the common element to result list variable
                             result.Add(nums2[i]);
-                            dict[nums2[i]] = count - 1;
+                            dict[nums2[i]] = counter - 1;
                         }
                         //Checking the value of key, if it is 1 adding that key element to list and removing it from list
-                        else if (count == 1)
+                        else if (counter == 1)
                         {
                             result.Add(nums2[i]);
                             dict.Remove(nums2[i]);
@@ -323,24 +295,19 @@ namespace Assignment2_Group6
                 //returing the common elements array
                 return result.ToArray();
             }
-            catch
-            {
-                throw;
-            }
-
+            catch { throw; }
         }
-
         public static bool ContainsDuplicate(char[] arr, int k)
         {
             try
             {
-                Dictionary<char, int> dict = new Dictionary<char, int>();   // declare character-integer dictionary 'dict'
+                Dictionary<char, int> dict = new Dictionary<char, int>();  // declare character-integer dictionary 'dict'
                 for (int i = 0; i < arr.Length; i++)
-                {                      // traverse through array length
+                {                                                           // traverse through array length
                     if (dict.ContainsKey(arr[i]))
-                    {                         // check if there is a value for arr[i]
+                    {                                                   // check if there is a value for arr[i]
                         if (i - dict[arr[i]] <= k)
-                        {                        // check if the difference is at most 'k'
+                        {                                               // check if the difference is at most 'k'
                             return true;
                         }
                     }
@@ -351,75 +318,53 @@ namespace Assignment2_Group6
             }
             catch (Exception) { throw; }                                    // throw en exception error
         }
-
         public static int GoldRod(int rodLength)
         {
             try
             {
-                if (rodLength == 2)
-                    return 1;
-                else if (rodLength == 3)
-                    return 2;
-                else if (rodLength == 4)
-                    return 4;
-                else if (rodLength == 5)
-                    return 6;
-                else if (rodLength == 6)
-                    return 9;
-                else
-                    return 3 * GoldRod(rodLength - 3);
-
+                if (rodLength == 2) return 1;               // check the 'rodLength' and return the corrosponding values
+                else if (rodLength == 3) return 2;
+                else if (rodLength == 4) return 4;
+                else if (rodLength == 5) return 6;
+                else if (rodLength == 6) return 9;
+                else return 3 * GoldRod(rodLength - 3);
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            catch (Exception) { throw; }
         }
-
-
         public static bool DictSearch(string[] userDict, string keyword)
         {
             try
             {
                 //bool state = false;
                 foreach (string words in userDict)
-                {
-                    int count = 0;
-                    
+                {                                                       // for each word in 'userDict' array
+                    int count = 0;                                      // set integer 'count' to 0
                     if (words.Length == keyword.Length)
-                    {
-                        char[] key = keyword.ToCharArray();
-                        char[] word = words.ToCharArray();
-
+                    {                                                   // if 'word' and 'keyword' are same
+                        char[] key = keyword.ToCharArray();             // save characters of 'keyword' in 'key'
+                        char[] word = words.ToCharArray();              // save characters of 'words' in 'word'
                         for (int i = 0; i < word.Length; i++)
-                        {
-                            if (word[i]!= key[i])
-                            {
-                                count = count + 1;
+                        {         // for 0 to word length
+                            if (word[i] != key[i])
+                            {                                           // check if index of word and key are same or not
+                                count = count + 1;                      // increase count
                             }
                         }
-
                         if (count == 1)
-                        {
-                            return true;
+                        {                                               // if count is 1
+                            return true;                                // return true
                             //break;
                         }
-                        
                     }
                 }
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            catch (Exception) { throw; }
             return default;
         }
-        
         public static void SolvePuzzle()
         {
             try
-            {
-                /*
+            {/*
                 //Console.WriteLine("Enter the first string(4):");
                 //string first_str = Console.ReadLine();
                 string first_str = "UBER";
@@ -435,20 +380,16 @@ namespace Assignment2_Group6
                 String final_str = first_str + second_str + result_str;
                 char[] char_arr = final_str.ToCharArray();
 
-                for (int i = 0; i < char_arr.Length; i++)
-                {
+                for (int i = 0; i < char_arr.Length; i++) {
                     Console.Write(char_arr[i]);
                     Console.WriteLine();
-                    if (unique_char.Contains(char_arr[i]))
-                    {
+                    if (unique_char.Contains(char_arr[i])) {
                         Console.WriteLine("Character repeat...");
                     }
-                    else 
-                    {
+                    else {
                         unique_char.Add(char_arr[i]);
                     }
                 }
-
                 foreach (int i in unique_char)
                     Console.Write(i);
                     */
@@ -465,10 +406,7 @@ namespace Assignment2_Group6
                 Console.WriteLine();
                 Console.WriteLine("U=1 B=2 E=7 R=4 C=9 O=6 L=3 N=0");
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            catch (Exception) { throw; }
         }
     }
 }
